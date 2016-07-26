f1a9991604cff1ce509d455f07fa9fa11ba8938c
Rakesh Verma <rkv160794@gmail.com>
2016-07-26 16:45:34 +0530

    Added one more test

--- src/Tests/GraphCreationTest.php
+++ src/Tests/GraphCreationTest.php
@@ -39,7 +39,17 @@ class GraphCreationTest extends MultiversionWebTestBase
     $this->tree = $this->container->get('multiversion.entity_index.rev.tree');
   }
 
-  public function testWithoutDelete()
+  /**
+   * Shape of Tree created is:
+   *              1
+   *            /   \
+   *           2     6
+   *         /   \
+   *        3     4
+   *             /
+   *            5
+   */
+  public function testGraphCreation()
   {
     $storage = $this->entityManager->getStorage('entity_test');
     $entity = $storage->create();
@@ -92,19 +102,109 @@ class GraphCreationTest extends MultiversionWebTestBase
     $vertices = $graph->getVertices()->getMap();
 
     foreach ($vertices[$revs[1]]->getVerticesEdgeFrom() as $parent) {
-      $this->assertEqual($parent->getId(), $revs[0],'node 1\'s parent is 0');
+      $this->assertEqual($parent->getId(), $revs[0],'node 2\'s parent is 1');
     }
     foreach ($vertices[$revs[2]]->getVerticesEdgeFrom() as $parent) {
-      $this->assertEqual($parent->getId(), $revs[1],'node 2\'s parent is 1');
+      $this->assertEqual($parent->getId(), $revs[1],'node 3\'s parent is 2');
     }
     foreach ($vertices[$revs[3]]->getVerticesEdgeFrom() as $parent) {
-      $this->assertEqual($parent->getId(),  $revs[1],'node 3\'s parent is 1');
+      $this->assertEqual($parent->getId(),  $revs[1],'node 4\'s parent is 2');
     }
     foreach ($vertices[$revs[4]]->getVerticesEdgeFrom() as $parent) {
-      $this->assertEqual($parent->getId(), $revs[3], 'node 4\'s parent is 3');
+      $this->assertEqual($parent->getId(), $revs[3], 'node 5\'s parent is 5');
     }
     foreach ($vertices[$revs[5]]->getVerticesEdgeFrom() as $parent) {
-      $this->assertEqual($parent->getId(), $revs[0], 'node 5\'s parent is 0');
+      $this->assertEqual($parent->getId(), $revs[0], 'node 6\'s parent is 1');
+    }
+  }
+
+  /**
+   *  Shape of tree is:
+   *            1
+   *          /   \
+   *         2     6
+   *        / \   / \
+   *       3   5 7   8
+   *      / \       /
+   *     4   9    10
+   *
+   */
+  public function testGraphCreation2()
+  {
+    $storage = $this->entityManager->getStorage('entity_test');
+    $entity = $storage->create();
+    $uuid = $entity->uuid();
+
+    // Create a conflict scenario to fully test the parsing.
+
+    // Initial revision.
+    $entity->save();
+    $revs[] = $entity->_rev->value;
+
+    $entity->save();
+    $revs[] = $entity->_rev->value;
+
+    $entity->save();
+    $revs[] = $entity->_rev->value;
+
+    $entity->save();
+    $revs[] = $entity->_rev->value;
+
+    // Create a new branch from the second revision.
+    $entity = $storage->loadRevision(2);
+    $entity->save();
+    $revs[] = $entity->_rev->value;
+
+    $entity = $storage->load(1);
+    $entity->save();
+    $revs[] = $entity->_rev->value;
+
+    $entity = $storage->loadRevision(6);
+    $entity->save();
+    $revs[] = $entity->_rev->value;
+
+    $entity = $storage->loadRevision(6);
+    $entity->save();
+    $revs[] = $entity->_rev->value;
+    // Continue the last branch.
+    $entity = $storage->loadRevision(3);
+    $entity->save();
+    $revs[] = $entity->_rev->value;
+    
+    // Create a new branch based on the first revision.
+    $entity = $storage->loadRevision(8);
+    $entity->save();
+    $revs[] = $entity->_rev->value;
+
+    $graph = $this->tree->getGraph($uuid);
+    $vertices = $graph->getVertices()->getMap();
+
+    foreach ($vertices[$revs[1]]->getVerticesEdgeFrom() as $parent) {
+      $this->assertEqual($parent->getId(), $revs[0],'node 2\'s parent is 1');
+    }
+    foreach ($vertices[$revs[2]]->getVerticesEdgeFrom() as $parent) {
+      $this->assertEqual($parent->getId(), $revs[1],'node 3\'s parent is 2');
+    }
+    foreach ($vertices[$revs[3]]->getVerticesEdgeFrom() as $parent) {
+      $this->assertEqual($parent->getId(),  $revs[1],'node 4\'s parent is 3');
+    }
+    foreach ($vertices[$revs[4]]->getVerticesEdgeFrom() as $parent) {
+      $this->assertEqual($parent->getId(), $revs[3], 'node 5\'s parent is 2');
+    }
+    foreach ($vertices[$revs[5]]->getVerticesEdgeFrom() as $parent) {
+      $this->assertEqual($parent->getId(), $revs[0], 'node 6\'s parent is 1');
+    }
+    foreach ($vertices[$revs[6]]->getVerticesEdgeFrom() as $parent) {
+      $this->assertEqual($parent->getId(), $revs[0],'node 7\'s parent is 6');
+    }
+    foreach ($vertices[$revs[7]]->getVerticesEdgeFrom() as $parent) {
+      $this->assertEqual($parent->getId(), $revs[1],'node 8\'s parent is 6');
+    }
+    foreach ($vertices[$revs[8]]->getVerticesEdgeFrom() as $parent) {
+      $this->assertEqual($parent->getId(),  $revs[1],'node 9\'s parent is 3');
+    }
+    foreach ($vertices[$revs[9]]->getVerticesEdgeFrom() as $parent) {
+      $this->assertEqual($parent->getId(), $revs[3], 'node 10\'s parent is 8');
     }
   }
 }
