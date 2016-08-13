4621d9daf749a1b60c19fe015d9ddba86aaa4f36
rakesh_verma <rkv160794@gmail.com>
2016-08-13 23:41:07 +0530

    Implementing merge resolver

--- src/Entity/Index/ComplexMergeResolver.php
+++ src/Entity/Index/ComplexMergeResolver.php
@@ -4,14 +4,22 @@ namespace Drupal\Multiversion\Entity\Index;
 
 use Drupal\conflict\ConflictResolverInterface;
 use Symfony\Component\Serializer;
-use Drupal\Serialization\Normalizer;
+use Drupal\Serialization\Normalizer\EntityNormalizer;
 use Drupal;
 use Relaxed\Merge\ThreeWayMerge;
 use Drupal\Core\Entity\RevisionableInterface;
 
+/**
+ * @property  container
+ */
 class ComplexMergeResolver implements ConflictResolverInterface {
 
-  
+  protected $serializer;
+
+  protected function setUp() {
+    $this->serializer = $this->container->get('serializer.normalizer.entity');
+  }
+
   /**
    * {@inheritdoc}
    */
@@ -27,9 +35,9 @@ class ComplexMergeResolver implements ConflictResolverInterface {
    * @return array
    */
   public function merge(RevisionableInterface $revision1, RevisionableInterface $revision2, RevisionableInterface $revision3) {
-    $r1_array = \Drupal::service('serializer')->normalize($revision1);
-    $r2_array = \Drupal::service('serializer')->normalize($revision2, 'array');
-    $r3_array = \Drupal::service('serializer')->normalize($revision3, 'array');
+    $r1_array = $this->serializer->normalize($revision1);
+    $r2_array = $this->serializer->normalize($revision2);
+    $r3_array = $this->serializer->normalize($revision3);
     $merge = new ThreeWayMerge();
     $result = $merge->performMerge($r1_array, $r2_array, $r3_array);
     return $result;
